43fe1287b88ec2007d109d8d9367b5745ddfc3a7
Chroma7p <nahco3.hcl@gmail.com>
2022-09-21 10:54:41 +0900

    hobbyページの更新

--- pages/hobby.tsx
+++ pages/hobby.tsx
@@ -11,13 +11,13 @@ const Hobby = () => {
           ここではよくプレイするものを挙げていますが、ゲームセンターで今動いているのは大体プレイしたことあります。
           時々レアな音楽ゲームをプレイしに行ったりもします。(Pump It
           Upやシンクロニカ、Musecaなど……)
-          <Block3 title="BeatmaniaIIDX">SP四段</Block3>
-          <Block3 title="Dance Dance Revolution">SP初段</Block3>
-          <Block3 title="jubeat">jubility 4900</Block3>
-          <Block3 title="SOUND VOLTEX">魔騎士 VOLFORCE 14.6</Block3>
+          <Block3 title="beatmania IIDX">SP五段</Block3>
+          <Block3 title="Dance Dance Revolution">SP七段/DP三段</Block3>
+          <Block3 title="jubeat">jubility 5000</Block3>
+          <Block3 title="SOUND VOLTEX">魔騎士 VOLFORCE 14.86</Block3>
           <Block3 title="NOSTALGIA">ベーシック3級 Grd.5700</Block3>
-          <Block3 title="CHUNITHM">MAX 16.30(虹)</Block3>
-          <Block3 title="オンゲキ">奏伝(15600) MAX 15.80(虹)</Block3>
+          <Block3 title="CHUNITHM">MAX 16.42(虹)</Block3>
+          <Block3 title="オンゲキ">奏伝(15763) MAX 15.80(虹)</Block3>
           <Block3 title="maimai">12000(銀)</Block3>
         </>
       </Block1>
