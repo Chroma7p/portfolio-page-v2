0e0c55542b1c3054572596c47715b2b45f197e89
Chroma7p <nahco3.hcl@gmail.com>
2023-01-29 00:43:15 +0900

    実績修正

--- pages/hobby.tsx
+++ pages/hobby.tsx
@@ -9,16 +9,15 @@ const Hobby = () => {
         <>
           技術的なこと以外では、アーケードの音楽ゲームが趣味です。色々なゲームを転々としながらプレイしています。
           ここではよくプレイするものを挙げていますが、ゲームセンターで今動いているのは大体プレイしたことあります。
-          時々レアな音楽ゲームをプレイしに行ったりもします。(Pump It
-          Upやシンクロニカ、Musecaなど……)
-          <Block3 title="beatmania IIDX">SP五段</Block3>
+          時々レアな音楽ゲームをプレイしに行ったりもします。(Pump It Upやシンクロニカ、Musecaなど……)
+          <Block3 title="beatmania IIDX">RESIDENT SP六段</Block3>
           <Block3 title="Dance Dance Revolution">SP七段/DP三段</Block3>
-          <Block3 title="jubeat">jubility 5000</Block3>
-          <Block3 title="SOUND VOLTEX">魔騎士 VOLFORCE 14.86</Block3>
+          <Block3 title="jubeat">Festo jubility 5500</Block3>
+          <Block3 title="SOUND VOLTEX">剛力羅 VOLFORCE 15.9</Block3>
           <Block3 title="NOSTALGIA">ベーシック3級 Grd.5700</Block3>
           <Block3 title="CHUNITHM">MAX 16.42(虹)</Block3>
-          <Block3 title="オンゲキ">奏伝(15763) MAX 15.80(虹)</Block3>
-          <Block3 title="maimai">12000(銀)</Block3>
+          <Block3 title="オンゲキ">奏伝(15770) MAX 15.80(虹)</Block3>
+          <Block3 title="maimai">UNIVERSE+ 12000ちょい(銀)</Block3>
         </>
       </Block1>
     </Base>
