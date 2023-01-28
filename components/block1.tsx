d99505db1bac5934ff13f3e1c293b7b61ea98996
Chroma7p <nahco3.hcl@gmail.com>
2023-01-29 01:44:09 +0900

    フォントサイズを大きく

--- components/block1.tsx
+++ components/block1.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const Block1 = ({ title, children }: Props) => {
   return (
     <div className="my-3 ml-10">
-      <h2 className="text-2xl border-b border-black">{title}</h2>
+      <h2 className="text-3xl border-b border-black">{title}</h2>
       <div className="mx-3 my-2">{children}</div>
     </div>
   );
--- components/block2.tsx
+++ components/block2.tsx
@@ -8,7 +8,7 @@ type Props = {
 export const Block2 = ({ title, children }: Props) => {
   return (
     <div className="mt-1 ml-10">
-      <h3 className="text-xl">{title}</h3>
+      <h3 className="text-2xl">{title}</h3>
       <div className="ml-5 my-2">{children}</div>
     </div>
   );
